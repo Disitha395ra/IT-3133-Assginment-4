c7f0f0eaf56b8f387081c5125c585b6768b10190
Disitha Ranasinghe <120271466+Disitha395ra@users.noreply.github.com>
2024-11-24 05:41:26 +0000

    add some inline styes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import {animals} from './data/AnimalsDb'
 import name from './components/name'
+import pictures from './components/pictures'
+import result from './components/result'
 
 function App() {
   return (
     <div>
       <h2>Animal maching game</h2>
       <div className="leftdiv">
-
+        <table>
+          <tr>
+            <result/>
+          </tr>
+        </table>
       </div>
       <div className="middlediv">
         <table>
@@ -17,7 +23,11 @@ function App() {
         </table>
       </div>
       <div classname="rightdiv">
-
+        <table>
+          <tr>
+            <pictures/>
+          </tr>
+        </table>
       </div>
     </div>
   );
--- my-app/src/components/result.js
+++ my-app/src/components/result.js
@@ -1,7 +1,7 @@
 export default function result(){
     return (
         <div>
-            
+            <h2>Result</h2>
         </div>
     )
 }
